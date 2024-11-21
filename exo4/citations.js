efdb7fdd34116d7c3446958376de2b50630c993b
Florian18 <florian.mancieri@gmail.com>
2024-11-21 16:20:48 +0100

    exercice 4
    publication exercice 4 citations

---
+++ exo4/citations.js
@@ -0,0 +1,16 @@
+// Tableau contenant les citations
+const citations = [
+    "La vie est ce qui arrive quand vous êtes occupé à faire d'autres projets. - John Lennon",
+    "Le succès, c’est tomber sept fois, se relever huit. - Proverbe japonais",
+    "Tout ce que vous pouvez imaginer est réel. - Pablo Picasso",
+    "Faites de votre vie un rêve, et d’un rêve, une réalité. - Antoine de Saint-Exupéry",
+    "Il n'y a qu'une façon d'échouer, c'est d'abandonner avant d'avoir réussi. - Georges Clemenceau",
+    "J'ai acheté un chateau ce n'est pas pour que les romanichelles s'installent sur le terrain"
+];
+
+// Récupération des éléments du DOM
+
+// Fonction pour générer une citation aléatoire
+
+
+// Ajout de l'événement au bouton
---
+++ exo4/readme.md
@@ -0,0 +1,4 @@
+Objectifs :
+Afficher une citation aléatoire chaque fois que l'utilisateur clique sur un bouton.
+Les citations doivent être stockées dans un tableau.
+Modifier dynamiquement le contenu de la page avec JavaScript.
